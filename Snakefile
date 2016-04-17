8b9f9bcf6f75810f173cfddd1beeb16fa0540f1d
Peter Edge <edge.peterj@gmail.com>
2016-04-16 19:16:36 -0700

    pipeline and matrix creation code ready for testing

--- Snakefile
+++ Snakefile
@@ -8,9 +8,15 @@
 # snakemake -j 200 --cluster-config cluster.yaml --cluster "qsub -A {cluster.group} -V -q {cluster.queue} -o {cluster.qsub_stdout_dir}/{params.job_name}.o -e {cluster.qsub_stdout_dir}/{params.job_name}.e -N {params.job_name} -l nodes=1:ppn={cluster.ppn} -l walltime={cluster.walltime} -M {cluster.email} -m e -d {cluster.working_dir}" --local-cores 1
 
 configfile: "config.yaml"
-localrules: simlink_PGP1_22, simlink_PGP1_21
+localrules: all, plot_hapcut2_results, simlink_PGP1_22, simlink_PGP1_21
+
+import sys
+import run_tools
+import plot_data
+sys.path.append(config['haptools_dir'])
 
 from create_hapcut_fragment_matrix import create_hapcut_fragment_matrix
+
 
 # qsub stderr and stdout directories are not automatically created by snakemake!
 if not os.path.exists(config['qsub_stdout_dir']):
@@ -18,62 +24,116 @@ if not os.path.exists(config['qsub_stdout_dir']):
 
 # specify chambers
 data_dir = config["data_dir"]
+experiments_dir = config["experiments_dir"]
+plots_dir = config["plots_dir"]
 variant_vcf_dir = config["variant_vcf_dir"]
 chambers = list(range(1,25))
 chambers_pad = ['{0:02d}'.format(c) for c in chambers]
 chroms   = list(range(1,23))
+samples = ['PGP1_21_22','PGP1_21','PGP1_22']
 
-rule make_PGP1_21_fragmat:
+rule all:
     input:
-        expand("{D}/PGP1_21/fragmat_all/chr{CHR}",D=data_dir,CHR=chroms)
+        expand("{P}/sissor_hapcut2.png",P=config['plots_dir'])
 
+# PLOT RESULTS
+rule plot_hapcut2_results:
+    params:
+        job_name = "plot_hapcut2_sissor"
+    input:
+        stats_file  = "{}/hapcut2.stats.p".format(config['error_rates_dir']),
+        labels_file = "{}/hapcut2.labels.p".format(config['error_rates_dir'])
+    output:
+        plot = expand("{P}/sissor_hapcut2.png",P=config['plots_dir'])
+    run:
+        data = pickle.load(open(input.stats_file,"rb"))
+        labels = pickle.load(open(input.labels_file,"rb"))
+        plot_data.plot_experiment_no_emphasis(data,labels,[],output.plot)
+
+# ERROR RATES
+rule calculate_error_rates:
+    params:
+        job_name = "hapcut2_error_rates"
+    input:
+        hapblocks = expand("{E}/hapcut_{s}/{c}.output",E=experiments_dir,s=samples,c=chroms),
+        runtimes = expand("{E}/hapcut_{s}/{c}.runtime",E=experiments_dir,s=samples,c=chroms),
+        expand("{{data_dir}}/{s}/fragmat/chr{c}",c=chroms),
+        expand("{v}/{c}.vcf", v=config['variant_vcf_dir'],c=chroms),
+        expand("{bac}/haplotype.chr{c}.bac.txt".format(bac=config['BAC_hapblocks'], c=chroms)
+    output:
+        stats_file  = "{}/hapcut2.stats.p".format(config['error_rates_dir']),
+        labels_file = "{}/hapcut2.labels.p".format(config['error_rates_dir'])
+    run:
+        # list of lists of error results,
+        data   = [] # index of the list is a 'condition' each inner list has 23 error results (each chrom).
+        labels = ['PGP1_21 + PGP1_22','PGP1_21','PGP1_22']
+        for s in samples: # sample
+            datalist = []
+            for c in chroms: # chromosome
+                assembly_file = "{}/hapcut2_{}/{}.output".format(config['experiments_dir'],s,c)
+                runtime_file  = "{}/hapcut2_{}/{}.runtime".format(config['experiments_dir'],s,c)
+                frag_file     = "{}/{}/fragmat/chr{}".format(d,s,c)
+                vcf_file      = "{}/chr{}.vcf".format(config['variant_vcf_dir'],c)
+                truth_file    = "{}/haplotype.chr{}.bac.txt".format(config['BAC_hapblocks'], c)
+                err = error_rates.hapblock_hapblock_error_rate(truth_file, assembly_file, frag_file, vcf_file, runtime_file)
+                datalist.append(err)
+
+            print("{} results over all chromosomes:".format(s))
+            print(sum(datalist,error_rates.error_result(None, 0, 0, 0, 0, 0, 0, 0, None,[],[],[])))
+
+            data.append(datalist)
+
+        pickle.dump(data,open(output.stats_file,"wb"))
+        pickle.dump(labels,open(output.labels_file,"wb"))
+
+# RUN HAPCUT2
+rule run_hapcut2:
+    params:
+        job_name = "{s}.{c}_hapcut",
+    input:
+        frag_file = "{data_dir}/{s}/fragmat/chr{c}",
+        vcf_file  = lambda wildcards: expand("{v}/{c}.vcf", v=config['variant_vcf_dir'],c=wildcards.c)
+    output:
+        hapblocks = "{E}/hapcut2_{s}/{c}.output",
+        runtime = "{E}/hapcut2_{s}/{c}.runtime"
+    run:
+        # run hapcut
+        runtime = run_tools.run_hapcut2(config['hapcut2'], input.frag_file, input.vcf_file, output.hapblocks, 100, 100, 1, 0, 15, 0.8)
+        with open(output.runtime,'w') as rf:
+            print(runtime, file=rf)
+
+# COMBINE FRAGMENT MATRICES
 rule merge_fragmat:
     params:
-        job_name    = "{I}.{CHR}_merge",
-        ppn         = "1",
-        walltime    = "1:00:00",
+        job_name  = "merge_fragmat",
+        ppn       = "1",
+        walltime  = "1:00:00",
     input:
-        expand("{{D}}/{{I}}/fragmat_sep/chamber{C}/chr{{CHR}}",C=chambers)
+        P21 = expand("{{data_dir}}/PGP1_21/fragmat/chr{CHR}",CHR=chroms),
+        P22 = expand("{{data_dir}}/PGP1_22/fragmat/chr{CHR}",CHR=chroms)
     output:
-        "{D}/{I}/fragmat_all/chr{CHR}"
+        P21_22 = expand("{{data_dir}}/PGP1_21_22/fragmat/chr{CHR}",CHR=chroms)
     run:
-        for i in input:
-            shell("cat {i} >> {output}")
+        for i1, i2, o in zip(P21, P22, P21_22):
+            shell('cat {i1} {i2} > {o}')
 
+# CREATE FRAGMENT MATRIX FILES FOR EXPERIMENT
 rule create_fragmat:
     params:
-        job_name  = "{I}_create_fragmat",
-        ppn       = "1",
+        job_name  = "{s}_create_fragmat",
+        ppn       = "4",
         walltime  = "20:00:00",
-        #V         = config["variant_vcf_dir"],
     input:
-        vcf         = expand("{{data_dir}}/{{I}}/ch{C}.vcf",C=chambers),
-        pileup      = expand("{{data_dir}}/{{I}}/ch{C}.pileup",C=chambers),
-        bed         = expand("{{data_dir}}/{{I}}/ch{C}.bed",C=chambers),
-        variant_vcf = "/oasis/tscc/scratch/pedge/sissor/PGP1_VCFs/chr{CHR}.vcf", # hardcoding this in temporarily. config dictionary was acting janky for no apparent reason
+        vcfs     = expand("{{data_dir}}/{{s}}/ch{C}.vcf",C=chambers),
+        pileups  = expand("{{data_dir}}/{{s}}/ch{C}.pileup",C=chambers),
+        beds     = expand("{{data_dir}}/{{s}}/ch{C}.bed",C=chambers),
+        var_vcfs = expand("{{data_dir}}/PGP1_VCFs/chr{CHR}.vcf",CHR=chroms)
     output:
-        expand("{{data_dir}}/{{I}}/fragmat_sep/chamber{C}/chr{{CHR}}",C=chambers)
+        expand("{{data_dir}}/{{s}}/fragmat/chr{CHR}",CHR=chroms)
     run:
-        for vcf, pileup, bed, variant_vcf, o in zip(input.vcf, input.pileup, input.bed, input.variant_vcf, output):
-            create_hapcut_fragment_matrix(vcf, bed, variant_vcf, pileup, o)
+        output_dir = os.path.join(data_dir,wildcards.s,'fragmat')
+        create_hapcut_fragment_matrix(input.vcfs, input.beds, input.pileups, input.var_vcfs, output_dir)
 
-'''
-rule create_fragmat:
-    params:
-        job_name  = "{I}.{C}_create_fragmat",
-        ppn       = "1",
-        walltime  = "20:00:00",
-        #V         = config["variant_vcf_dir"],
-    input:
-        vcf         = "{data_dir}/{I}/ch{C}.vcf",
-        pileup      = "{data_dir}/{I}/ch{C}.pileup",
-        bed         = "{data_dir}/{I}/ch{C}.bed",
-        variant_vcf = "/oasis/tscc/scratch/pedge/sissor/PGP1_VCFs/chr{CHR}.vcf", # hardcoding this in temporarily. config dictionary was acting janky for no apparent reason
-    output:
-        "{data_dir}/{I}/fragmat_sep/chamber{C}/chr{CHR}"
-    run:
-        create_hapcut_fragment_matrix(input.vcf, input.bed, input.variant_vcf, input.pileup, output)
-'''
 # simlink data to make path naming scheme consistent between PGP1_21 and PGP_22
 rule simlink_PGP1_21:
     input:
@@ -88,8 +148,7 @@ rule simlink_PGP1_21:
             mkdir -p {data_dir}/PGP1_21
             ln -s {config[PGP_21_vcfs_old]}/PGP1_21_ch{C}.sorted.fragment.depth5.vcf {data_dir}/PGP1_21/ch{c}.vcf
             ln -s {config[PGP_21_pileups_old]}/PGP1_21_ch{C}.sorted.fragment.depth.pileup {data_dir}/PGP1_21/ch{c}.pileup
-            ln -s {config[PGP_21_boundaries_old]}/PGP1_21_FragmentBoundaryCh{c}.bed {data_dir}/PGP1_21/ch{c}.bed
-            ''')
+            ln -s {config[PGP_21_boundaries_old]}/PGP1_21_FragmentBoundaryCh{c}.bed {data_dir}/PGP1_21/ch{c}.bed''')
 
 # simlink data to make path naming scheme consistent between PGP1_21 and PGP_22
 rule simlink_PGP1_22:
@@ -105,5 +164,4 @@ rule simlink_PGP1_22:
             mkdir -p {data_dir}/PGP1_22
             ln -s {config[PGP_22_old]}/PGP1_22_ch{C}.sorted.fragment.vcf {data_dir}/PGP1_22/ch{c}.vcf
             ln -s {config[PGP_22_old]}/PGP1_22_ch{C}.sorted.fragment.depth.pileup {data_dir}/PGP1_22/ch{c}.pileup
-            ln -s {config[PGP_22_old]}/PGP1_22_FragmentBoundaryCh{c}.bed {data_dir}/PGP1_22/ch{c}.bed
-            ''')
+            ln -s {config[PGP_22_old]}/PGP1_22_FragmentBoundaryCh{c}.bed {data_dir}/PGP1_22/ch{c}.bed''')
--- create_hapcut_fragment_matrix.py
+++ create_hapcut_fragment_matrix.py
@@ -4,8 +4,10 @@
 # Author : Peter Edge
 # Email  : pedge@eng.ucsd.edu
 import argparse
+import os
 import sys
 import math
+from collections import defaultdict
 
 desc = '''
 Converts SISSOR haploid vcf files to hapcut fragment matrices.
@@ -23,10 +25,10 @@ def parse_args():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-s','--sissor_vcf_files', nargs='+', type = str, help='path to SISSOR haploid vcf file containing only non-reference calls',default=[])
     parser.add_argument('-b','--bed_files', nargs='+', type = str, help='bed file where each entry specifies the boundary of a SISSOR fragment',default=[])
-    parser.add_argument('-v','--variant_vcf_file', nargs='?', type = str, help='vcf file containing reliable variant calls to be used with HapCUT')
     parser.add_argument('-p','--pileup_files', nargs='+', type = str, help='oldschool samtools pileup file from SISSOR raw reads',default=[])
-    parser.add_argument('-c','--condensed_pileup', action='store_true', help='set this flag if using special 3 column condensed pileup')    
-    parser.add_argument('-o','--output_fragment_file', nargs='?', type = str, help='path to result fragment file')
+    parser.add_argument('-v','--variant_vcf_files', nargs='+', type = str, help='vcf file containing reliable variant calls to be used with HapCUT',default=[])
+    parser.add_argument('-o','--output_dir', nargs='?', type = str, help='directory to output fragment files')
+    parser.add_argument('-c','--condensed_pileup', action='store_true', help='set this flag if using special 3 column condensed pileup')
     # default to help option. credit to unutbu: http://stackoverflow.com/questions/4042452/display-help-message-with-python-argparse-when-script-is-called-without-any-argu
     if len(sys.argv) < 5:
         parser.print_help()
@@ -35,11 +37,34 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def create_hapcut_fragment_matrix(sissor_vcf_files, bed_files, variant_vcf_file, pileup_files, output_fragment_file, condensed_pileup=False):
+def create_hapcut_fragment_matrices(sissor_vcf_files, bed_files, variant_vcf_files, pileup_files, output_dir, condensed_pileup=False):
 
-    linelist = [] # list of tuples that map first snp pos to line
+    if not os.path.exists(output_dir):
+        os.path.makedirs(output_dir)
+
+    lines = defaultdict(list) # lines[chrom] contains a list of (first_index, line) tuples. these will be sorted by first pos and printed to fragment outfile
+    snp_indices = dict() # chrom -> list of snp indices
+    ix_counter = defaultdict(lambda: 1)
+    
+    # need to define the columns of the fragment matrix (indexes and positions of SNPs)
+    # use a vcf file as a template to know what SNP indices are    
+    for variant_vcf_file in variant_vcf_files:
+        with open(variant_vcf_file, 'r') as infile:
+    
+            for line in infile:
+                if (line[0] == '#' or len(line) < 2):
+                    continue
+                el = line.strip().split()
+                chrom = el[0]
+                pos   = int(el[1])-1
+                snp_indices[(chrom,pos)] = ix_counter[chrom] # given a chromosome and genomic index, return the SNP index within that chroms vcf
+                ix_counter[chrom] += 1
         
+    # step through triples of a haploid vcf file, the bedfile that defines
+    # where fragment boundaries are, and the pileup file.
+    # pileup file allows us to know which SNPs have acceptable depth/quality
     for sissor_vcf_file, bed_file, pileup_file in zip(sissor_vcf_files, bed_files, pileup_files):
+        
         # read bed file boundaries into a list structure
         bed_data = []
         with open(bed_file, 'r') as infile:
@@ -47,31 +72,10 @@ def create_hapcut_fragment_matrix(sissor_vcf_files, bed_files, variant_vcf_file,
                 if len(line) < 2:
                     continue
                 bed_data.append(line.strip().split())
-    
-        # create a list of (chromosome, pos) tuples specifying each allele present in hapcut haplotype
-        snp_indices_unsorted = []
-        # want a set too for fast lookup
-        snp_indices_set      = set()
-    
-        # need to define the columns of the fragment matrix (indexes and positions of SNPs)
-        # use a vcf file as a template to know where SNPs and their indices are
-        with open(variant_vcf_file, 'r') as infile:
-    
-            ix = 1
-            for line in infile:
-                if (line[0] == '#' or len(line) < 2):
-                    continue
-                el = line.strip().split()
-                chrom = el[0]
-                pos   = int(el[1])-1
-                snp_indices_unsorted.append((ix,chrom,pos))
-                snp_indices_set.add((chrom,pos))
-                ix += 1
-    
-        snp_indices = sorted(snp_indices_unsorted, key=lambda tup: tup[0])
-    
-        covered = dict()
 
+        # make a "Q_dict" dictionary to indicate if a position is sufficiently
+        # covered by reads and what its quality score is
+        Q_dict = dict()
         if condensed_pileup:
             
             with open(pileup_file, 'r') as infile:
@@ -88,7 +92,7 @@ def create_hapcut_fragment_matrix(sissor_vcf_files, bed_files, variant_vcf_file,
                     
                     q_char = '~' if qual < 5.011872336272714e-10 else chr(int(33-10*math.log10(qual)))
 
-                    covered[(chrom,pos)] = q_char
+                    Q_dict[(chrom,pos)] = q_char
         
         else:
             with open(pileup_file, 'r') as infile:
@@ -99,39 +103,16 @@ def create_hapcut_fragment_matrix(sissor_vcf_files, bed_files, variant_vcf_file,
                     el    = line.strip().split()
                     chrom = el[0]
                     pos   = int(el[1])-1
-                    qual  = float(el[4])
+                    qual  = int(el[4])
                     depth = int(el[7])
                     Q = 10**(qual*-0.1)
-    
+                    q_char = '~' if qual>=126 else chr(33 + qual)
+                    
                     # filter on quality and depth
                     # we only care about this position if it's also in the HapCUT block file
-                    if qual >= 30 and depth >= 5 and (chrom, pos) in snp_indices_set:
-                        covered[(chrom,pos)] = Q
+                    if qual >= 30 and depth >= 5 and (chrom, pos) in snp_indices:
+                        Q_dict[(chrom,pos)] = q_char
 
-                                
-        # create a list where each element is a list corresponding to a bed region
-        # each A contains tuples for each snp found in the hapcut haplotype that falls in the region
-        frag_snps = [[] for i in bed_data]
-        names     = []
-    
-        for i, (chrom, p1, p2, l, chamber) in enumerate(bed_data):
-            p1 = int(p1)
-            p2 = int(p2)
-            curr_name = '{}:{}-{}'.format(chrom[3:],p1,p2)
-            names.append(curr_name)
-            if snp_indices == []:
-                break
-            snp_ix, snp_chrom, snp_pos = snp_indices.pop(0)
-            # consume SNPs leading up to bed region
-            while (snp_chrom != chrom or snp_pos < p1) and snp_indices != []:
-                snp_ix, snp_chrom, snp_pos = snp_indices.pop(0)
-            # add SNPs inside current bed region to fs
-            while snp_chrom == chrom and p1 <= snp_pos and snp_pos <= p2 and snp_indices != []:
-                if (snp_chrom, snp_pos) in covered:
-                    Q = covered[(snp_chrom, snp_pos)]
-                    frag_snps[i].append((snp_ix, snp_chrom, snp_pos, '0', Q))
-                snp_ix, snp_chrom, snp_pos = snp_indices.pop(0)
-    
         # create a set of indices from the haploid SISSOR VCF where the non-reference alleles are found
     
         nonrefs = set()
@@ -147,30 +128,41 @@ def create_hapcut_fragment_matrix(sissor_vcf_files, bed_files, variant_vcf_file,
                 chrom = el[0]
                 pos   = int(el[1])-1
                 nonrefs.add((chrom,pos))
+                           
+        # create a list where each element is a list corresponding to a bed region
+        # each A contains tuples for each snp found in the hapcut haplotype that falls in the region
+        frag_snps = [[] for i in bed_data]
+        names     = []
     
-        # go through our list of fragment SNP indices and use the set of nonref alleles
-        # to mark each nonreference position as such
-        for i in range(0,len(frag_snps)):
-    
-            for j in range(0,len(frag_snps[i])):
-    
-                snp_ix, chrom, pos, allele, Q = frag_snps[i][j]
-    
-                if (chrom, pos) in nonrefs:
-                    frag_snps[i][j] = (snp_ix, chrom, pos, '1', Q) # mark this as a nonref allele
-    
-    
-        # now take this information and print it to a fragment matrix file
-        
-    
+        for i, (chrom, p1, p2, l, chamber) in enumerate(bed_data):
+            p1 = int(p1)
+            p2 = int(p2)
+            curr_name = '{}:{}-{}'.format(chrom,p1,p2)
+            names.append(curr_name)
+            # consume SNPs leading up to bed region
+            # add SNPs inside current bed region to fs
+            for snp_pos in range(p1,p2+1):
+                if (chrom, snp_pos) not in Q_dict or (chrom, snp_pos) not in snp_indices:
+                    continue
+                
+                Q = Q_dict[(chrom, snp_pos)] # get the quality char from the genotype call
+                snp_ix = snp_indices[(chrom, snp_pos)]
+                
+                if (chrom, snp_pos) in nonrefs:
+                    frag_snps[i].append((snp_ix, chrom, snp_pos, '1', Q)) # mark this as a nonref allele
+                else:
+                    frag_snps[i].append((snp_ix, chrom, snp_pos, '0', Q)) # mark this as a ref allele
+                
+        # now take this information and make it into a fragment matrix file line
         for name, fs in zip(names, frag_snps):
-            if fs == []:
+            if len(fs) < 2:
                 continue
             
             fragstr = ''
             num_pairs = 0
             prev_snp_ix = -2
             qual = ''
+            chrom    = fs[0][1]
             firstpos = fs[0][2]
             for snp_ix, chrom, pos, allele, q_char in fs:
     
@@ -187,20 +179,24 @@ def create_hapcut_fragment_matrix(sissor_vcf_files, bed_files, variant_vcf_file,
     
             fragstr += ' ' + ''.join(qual)
     
-            prefix = '{} {} '.format(num_pairs,name)
+            prefix = '{} {}'.format(num_pairs,name)
             fragstr = prefix + fragstr
             
-            linelist.append((firstpos, fragstr))
-    
-    
-    linelist.sort()
-                
-    with open(output_fragment_file, 'w') as opf:
-        for firstpos, line in linelist:
-            print(line, file=opf)
+            lines[chrom].append((firstpos, fragstr))
+        
+    # go through the output lines we've accrued for each chromosome.
+    # sort them and print them to a fragment file for each chromosome.
+    for chrom in lines.keys():
+        lines[chrom].sort()
+        
+        output_fragment_file = os.path.join(output_dir, chrom)            
+        
+        with open(output_fragment_file, 'w') as opf:
+            for firstpos, line in lines[chrom]:
+                print(line, file=opf)
 
             
 # parse args and execute main function
 if __name__ == '__main__':
     args = parse_args()
-    create_hapcut_fragment_matrix(args.sissor_vcf_files, args.bed_files, args.variant_vcf_file, args.pileup_files, args.output_fragment_file, args.condensed_pileup,)
+    create_hapcut_fragment_matrices(args.sissor_vcf_files, args.bed_files, args.variant_vcf_files, args.pileup_files, args.output_dir, args.condensed_pileup)
