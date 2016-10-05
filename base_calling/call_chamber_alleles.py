b592d87205a97d2d43da17ec813aa9445c848391
Peter Edge <edge.peterj@gmail.com>
2016-10-04 20:35:13 -0700

    syncing to tscc

--- base_calling/call_chamber_alleles.py
+++ base_calling/call_chamber_alleles.py
@@ -8,6 +8,7 @@ from pdb import set_trace
 if False:
     set_trace() # to dodge warnings that pdb isn't being used.
 import time
+import re
 import pickle
 import copy
 #import sys
@@ -363,46 +364,68 @@ def pr_allele(cell, chamber, pr_one_ch, nonzero_chambers, mixed_allele_priors, r
 ###############################################################################
 
 
+caret_money = re.compile(r'\^.|\$')
+comma_or_period = re.compile(r'\.|\,')
+plus_or_minus = re.compile(r'\+|-')
+
 def parse_mpileup_base_qual(raw_bd, raw_qd, ref_base):
 
-    i = 0   # index for base data
-    j = 0   # index for qual data
     bd = [] # base data
     qd = [] # qual data
+    
+    if not re.search(plus_or_minus,raw_bd):
 
-    while i < len(raw_bd):
-        if raw_bd[i] == '.' or raw_bd[i] == ',':   # reference base call
-            bd.append(ref_base)
-            qd.append(10**((ord(raw_qd[j]) - 33) * -0.1))
-            i += 1
-            j += 1
-        elif raw_bd[i] in base_letters:            # variant call
-            bd.append(str.upper(raw_bd[i]))
-            qd.append(10**((ord(raw_qd[j]) - 33) * -0.1))
-            i += 1
-            j += 1
-        elif raw_bd[i] == '+' or raw_bd[i] == '-': # indel
-            num = int(raw_bd[i+1])
-            i += 2
-            while(raw_bd[i] in numbers):
-                num *= 10
-                num += int(raw_bd[i])
+        bd = str.upper(re.sub(caret_money,'', raw_bd))
+        bd = re.sub(comma_or_period, ref_base, bd)
+        qd = [10**((ord(q) - 33) * -0.1) for q in raw_qd]
+        paired_bd_qd = [(b,q) for b,q in zip(bd,qd) if b not in ['>','<','*','n','N']]
+        if len(paired_bd_qd) < coverage_cut:
+            return [],[]
+        
+        bd, qd = zip(*paired_bd_qd)
+        bd = list(bd)
+        qd = list(qd)
+    
+    else:
+        
+        i = 0   # index for base data
+        j = 0   # index for qual data
+    
+        while i < len(raw_bd):
+            if raw_bd[i] == '.' or raw_bd[i] == ',':   # reference base call
+                bd.append(ref_base)
+                qd.append(10**((ord(raw_qd[j]) - 33) * -0.1))
                 i += 1
-            i += num
-        elif raw_bd[i] == '^':
-            i += 2
-        elif raw_bd[i] == '$':
-            i += 1
-        elif raw_bd[i] in '><*':                 # reference skip or deletion
-            i += 1
-            j += 1
-
-    assert(i == len(raw_bd))
-    assert(j == len(raw_qd))
+                j += 1
+            elif raw_bd[i] in base_letters:            # variant call
+                bd.append(str.upper(raw_bd[i]))
+                qd.append(10**((ord(raw_qd[j]) - 33) * -0.1))
+                i += 1
+                j += 1
+            elif raw_bd[i] == '+' or raw_bd[i] == '-': # indel
+                num = int(raw_bd[i+1])
+                i += 2
+                while(raw_bd[i] in numbers):
+                    num *= 10
+                    num += int(raw_bd[i])
+                    i += 1
+                i += num
+            elif raw_bd[i] == '^':
+                i += 2
+            elif raw_bd[i] == '$':
+                i += 1
+            elif raw_bd[i] in '><*nN':                 # reference skip or deletion
+                i += 1
+                j += 1
+    
+        assert(i == len(raw_bd))
+        assert(j == len(raw_qd))
+        
     assert(len(bd) == len(qd))
 
     for b in bd:
         assert(b in bases)
+
 
     return bd, qd
     
@@ -451,12 +474,12 @@ def call_chamber_alleles(input_file, output_file, SNPs_only=True):
 
                     bd, qd = parse_mpileup_base_qual(raw_bd, raw_qd, ref_base)
 
+                    if len(bd) < coverage_cut:
+                        continue
+                    
                     for b in bd:
                         if b != ref_base:
                             total_nonref += 1
-
-                    if len(bd) < coverage_cut:
-                        continue
 
                     base_data[cell_num][ch_num] = bd[:max_cov]
                     qual_data[cell_num][ch_num] = qd[:max_cov]
@@ -496,7 +519,7 @@ def call_chamber_alleles(input_file, output_file, SNPs_only=True):
 if __name__ == '__main__':
     t1 = time.time()
     args = parseargs()
-    call_chamber_alleles(args.input_file, args.output_file)
+    call_chamber_alleles(args.input_file, args.output_file,False)
     t2 = time.time()
 
     print("TOTAL TIME: {} s".format(int(t2-t1)))
