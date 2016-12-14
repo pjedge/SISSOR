7cca409931a7ad30420feedd332940d63341431e
Peter Edge <edge.peterj@gmail.com>
2016-12-13 19:20:59 -0800

    initial commit for haplotype-aware SISSOR base calling. Firstly, implemented a fast variant of normal base caller that doesn't use cross chamber information. This could be used for initial phasing. Secondly, began work implementing haplotype-matching into the cross-chamber caller. Currently in testing mode and requires specification of which chambers are in phase or not in phase (or unknown) with respect to one another.

--- base_calling/sissorhands.py
+++ base_calling/sissorhands.py
@@ -8,6 +8,7 @@ if False:
 import time
 import re
 import pickle
+from collections import defaultdict
 
 #from numpy import logaddexp as addlogs
 
@@ -28,7 +29,7 @@ desc = 'Use cross-chamber information to call chamber alleles in SISSOR data'
 cells = ['PGP1_21','PGP1_22','PGP1_A1']
 coverage_cut = 2 # less than this many reads in a chamber are ignored
 max_cov = 100
-min_nonref = 5
+min_nonref = 3
 chroms = ['chr{}'.format(i) for i in range(1,23)] + ['chrX','chrY']
 n_chambers = 24
 chambers = list(range(1,n_chambers+1))
@@ -50,14 +51,16 @@ for i,chrom in enumerate(chroms):
 # DEFAULTS...
 ###############################################################################
 
-default_input_file = 'test_subsample.txt'
-default_output_file = 'test_subsample.output'
+default_input_file = 'pileup_example.txt'#'test_subsample.txt'
+default_output_file =  'output.txt'#'test_subsample.output'
+BASIC = True
 
-default_fragment_boundaries = []
-for cell in cells:
-    for chamber in chambers:
-        filename = 'fragment_boundary_beds/{}/ch{}.bed'.format(cell,chamber)
-        default_fragment_boundaries.append(filename)
+default_fragment_boundaries = None
+#default_fragment_boundaries = []
+#for cell in cells:
+#    for chamber in chambers:
+#        filename = 'fragment_boundary_beds/{}/ch{}.bed'.format(cell,chamber)
+#        default_fragment_boundaries.append(filename)
 
 
 ###############################################################################
@@ -70,7 +73,7 @@ def parseargs():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-i', '--input_file', nargs='?', type = str, help='input file, pileup with n cells x 24 chambers', default=default_input_file)
     parser.add_argument('-o', '--output_file', nargs='?', type = str, help='file to write output to', default=default_output_file)
-    parser.add_argument('-b', '--fragment_boundaries', nargs='+', type = str, help='', default=default_fragment_boundaries)
+    parser.add_argument('-b', '--fragment_boundaries', nargs='+', type = str, help='', default=None)
     parser.add_argument('-n', '--num_cells', nargs='?', type = int, help='number of cells being analyzed, default: 3', default=3)
 
     args = parser.parse_args()
@@ -96,6 +99,9 @@ omega_nolog = 10**omega
 MDA_ERR = 1e-5 # the probability of consensus error due to MDA
 MDA_COR = log10(1 - MDA_ERR)
 MDA_ERR = log10(MDA_ERR) - log10(3) # the log probability of an MDA error, divided by 3
+
+INTRACELL_HAP_PENALTY = log10(1e-10) # intra-cell penalty for assigning chambers of different haps to same haps
+INTERCELL_HAP_PENALTY = log10(1e-5) # inter-cell penalty for assigning chambers of different haps to same haps
 
 # INPUT
 # G: a tuple that specifies genotype e.g. ('A','T')
@@ -337,7 +343,7 @@ def precompute_pr_one_chamber(base_data, qual_data, nonzero_chambers, fast_mode)
 # qual_data: list length 24, containing 1 list per chamber. inner list has q values (p(base call error)) callin in chamber.
 # configs:   list of configurations and their probabilities, see earlier
 
-def pr_genotype(pr_one_ch, nonzero_chambers, mixed_allele_priors, ref_allele, condensed_genotype_set):
+def pr_genotype(pr_one_ch, nonzero_chambers, ref_allele, condensed_genotype_set):
 
     probs = dict()
     outline_el = ['*'] + ['*'] * (n_cells * n_chambers)
@@ -430,6 +436,75 @@ def pr_genotype(pr_one_ch, nonzero_chambers, mixed_allele_priors, ref_allele, co
 
                     assignments.add((i,j,alleles_present))
 
+
+            ########### NEW, 10/13/2016 #######################################
+            # IF WE KNOW TWO CHAMBERS c_a, c_b BELONG TO SAME HAPLOTYPE
+            # incur a penalty for assigning those to non-complementary strands
+            # IF WE KNOW TWO CHAMBERS c_a, c_b BELONG TO DIFFERENT HAPLOTYPE
+            # incur a penalty for assigning those to complementary strands
+            
+            # this penalty should be large within-cell and slightly smaller between cell.
+            # also, a chamber should get a special tag denoting its status:
+            # SAME CELL SAME HAP
+            # SAME CELL DIFFERENT HAP
+            # DIFFERENT CELL SAME HAP
+            # DIFFERENT CELL DIFFERENT HAP
+            same_haplotype = defaultdict(lambda: None) ## need to build this
+            same_haplotype[(0,1),(0,2)] = True
+            same_haplotype[(0,2),(0,1)] = True
+            done = set()
+            
+            # TODO: strand-matched chambers with the same data get different posteriors, figure out why            
+            
+            # ADDRESS THIS ISSUE, bundled/multicounted configurations
+            
+            #if c1 < c2 and c3 < c4:
+            #    perm = log10(4)
+            #elif (c1 < c2 and c3 == c4) or (c1 == c2 and c3 < c4):
+            #    perm = log10(2)
+            #elif c1 == c2 and c3 == c4:
+            #    perm = log10(1)    
+            
+            
+            for CELL1 in range(0,n_cells):
+                cell1_chambers, p_cell1_cfg = config[CELL1]
+                
+                for CELL2 in range(0,n_cells):
+                    cell2_chambers, p_cell2_cfg = config[CELL2]
+
+                    for x1,y1 in enumerate(cell1_chambers):
+                        for x2, y2 in enumerate(cell2_chambers):
+                            
+                            if x1 == n_chambers or x2 == n_chambers: # assignments to null chamber (not sampled)
+                                continue
+                            
+                            if ((CELL1,x1),(CELL2,x2)) in done or ((CELL2,x2),(CELL1,x1)) in done:
+                                continue
+                            
+                            if CELL1 == CELL2 and y1 == y2:
+                                continue
+
+                            if same_haplotype[(CELL1,x1),(CELL2,x2)] == None:
+                                continue # no haplotype information between these chambers   
+                            
+                            if ((same_haplotype[(CELL1,x1),(CELL2,x2)]
+                             and not ((y1 < 2 and y2 < 2) or (y1 >= 2 and y2 >= 2)))
+                             or ((not same_haplotype[(CELL1,x1),(CELL2,x2)])
+                                and ((y1 < 2 and y2 < 2) or (y1 >= 2 and y2 >= 2)))):   # 0,1 -> parent1 , 2,3 -> parent2
+                                
+                                # same haplotype but this configuration assigns them to different haplotypes
+                                # or diff haplotype but this configuration assigns them to same haplotypes
+
+                                if CELL1 == CELL2:
+                                    p += INTRACELL_HAP_PENALTY
+                                else: 
+                                    p += INTERCELL_HAP_PENALTY
+                                    
+                            done.add(((CELL1,x1),(CELL2,x2)))
+                
+            ###################################################################
+
+
             p_total = addlogs(p_total, p) # update genotype likelihood sum
 
             for assignment in assignments:
@@ -468,11 +543,52 @@ def pr_genotype(pr_one_ch, nonzero_chambers, mixed_allele_priors, ref_allele, co
     for cell in range(0,n_cells):  # for each cell
         for chamber in nonzero_chambers[cell]:
 
+            #num_nonzero = len(nonzero_chambers[cell])
+            #pr_all_chamber_data(allele, ref_allele, cell, chamber, pr_one_ch, nonzero_chambers, hom_het_configs)
+            probs = []
+            for allele in mixed_alleles:
+                prob = p_assignment[(cell,chamber,allele)] #+ mixed_allele_priors[ref_allele][num_nonzero][allele]
+                probs.append((allele,prob))
+
+            # denominator for bayes rule posterior calculation
+            total = tinylog
+            for a,p in probs:
+                total = addlogs(total,p)
+
+            res = []
+            max_allele = ('N',)
+            max_posterior = -1
+            for a,p in probs:
+                posterior = 10**(p - total)
+                res.append((a,posterior))
+
+                if posterior > max_posterior:
+                    max_posterior = posterior
+                    max_allele = a
+
+            if max_allele != (ref_allele,):
+                SNP = True
+
+            out_str = ';'.join(['{}:{}'.format(''.join(a),p) for a,p in res])
+
+            outline_el[1 + cell*n_chambers + chamber] = out_str
+
+    return outline_el, SNP
+
+
+def pr_chambers_BASIC(pr_one_ch, nonzero_chambers, mixed_allele_priors, ref_allele, condensed_genotype_set):
+
+    probs = dict()
+    outline_el = ['*'] + ['*'] * (n_cells * n_chambers)
+
+    for cell in range(0,n_cells):  # for each cell
+        for chamber in nonzero_chambers[cell]:
+
             num_nonzero = len(nonzero_chambers[cell])
             #pr_all_chamber_data(allele, ref_allele, cell, chamber, pr_one_ch, nonzero_chambers, hom_het_configs)
             probs = []
             for allele in mixed_alleles:
-                prob = p_assignment[(cell,chamber,allele)] + mixed_allele_priors[ref_allele][num_nonzero][allele]
+                prob = pr_one_ch[(cell,chamber,allele)] + mixed_allele_priors[ref_allele][num_nonzero][allele]
                 probs.append((allele,prob))
 
             # denominator for bayes rule posterior calculation
@@ -503,7 +619,6 @@ def pr_genotype(pr_one_ch, nonzero_chambers, mixed_allele_priors, ref_allele, co
 ###############################################################################
 # MAIN FUNCTION AND PARSING
 ###############################################################################
-
 
 caret_money = re.compile(r'\^.|\$')
 comma_or_period = re.compile(r'\.|\,')
@@ -593,14 +708,15 @@ def parse_bedfile(input_file):
     return boundaries
 
 # boundary_files[cell*chamber+chamber] should hold name of bed file with fragment boundaries
+# use no_cross_chamber_info to ignore cross-chamber information
+def call_chamber_alleles(input_file, output_file, boundary_files=None, SNPs_only=False, no_cross_chamber_info=False, minimum_coverage=5):
 
-def call_chamber_alleles(input_file, output_file, boundary_files, SNPs_only=False):
-
-    fragment_boundaries = [[] for i in range(n_cells)]
-    for cell in range(0,n_cells):  # for each cell
-        for chamber in range(0,n_chambers):
-            bfile = boundary_files[cell*n_chambers + chamber]
-            fragment_boundaries[cell].append(parse_bedfile(bfile))
+    if boundary_files != None:
+        fragment_boundaries = [[] for i in range(n_cells)]
+        for cell in range(0,n_cells):  # for each cell
+            for chamber in range(0,n_chambers):
+                bfile = boundary_files[cell*n_chambers + chamber]
+                fragment_boundaries[cell].append(parse_bedfile(bfile))
 
     mixed_allele_priors = compute_mixed_allele_priors()
     processed = 0
@@ -630,28 +746,29 @@ def call_chamber_alleles(input_file, output_file, boundary_files, SNPs_only=Fals
                 for ch_num in range(n_chambers):
 
                     # ensure that position falls inside a called fragment
-                    if fragment_boundaries[cell_num][ch_num] == []:
-                        continue
-
-                    f_chrom, f_start, f_end = fragment_boundaries[cell_num][ch_num][0]
-
-                    # if we're behind fragment start, skip this spot
-                    if chr_num[chrom] < chr_num[f_chrom] or (chrom == f_chrom and pos < f_start):
-                        continue
-
-                    # if we're ahead of fragment start, skip to later fragment boundaries
-                    while 1:
+                    if boundary_files != None:
                         if fragment_boundaries[cell_num][ch_num] == []:
-                            break
+                            continue
+                    
                         f_chrom, f_start, f_end = fragment_boundaries[cell_num][ch_num][0]
-                        if chr_num[chrom] > chr_num[f_chrom] or (chrom == f_chrom and pos >= f_end):
-                            fragment_boundaries[cell_num][ch_num].pop(0)
-                        else:
-                            break
 
-                    # if we're not inside fragment, continue
-                    if not(chrom == f_chrom and pos >= f_start and pos < f_end):
-                        continue
+                        # if we're behind fragment start, skip this spot
+                        if chr_num[chrom] < chr_num[f_chrom] or (chrom == f_chrom and pos < f_start):
+                            continue
+
+                        # if we're ahead of fragment start, skip to later fragment boundaries
+                        while 1:
+                            if fragment_boundaries[cell_num][ch_num] == []:
+                                break
+                            f_chrom, f_start, f_end = fragment_boundaries[cell_num][ch_num][0]
+                            if chr_num[chrom] > chr_num[f_chrom] or (chrom == f_chrom and pos >= f_end):
+                                fragment_boundaries[cell_num][ch_num].pop(0)
+                            else:
+                                break
+    
+                        # if we're not inside fragment, continue
+                        if not(chrom == f_chrom and pos >= f_start and pos < f_end):
+                            continue
 
                     flat_ix = cell_num * n_chambers + ch_num
                     col_ix = 3 + 4 * flat_ix
@@ -733,10 +850,20 @@ def call_chamber_alleles(input_file, output_file, boundary_files, SNPs_only=Fals
             if nonzero_chamber_count > 0 and not too_many_chambers:
 
                 pr_one_ch = precompute_pr_one_chamber(base_data, qual_data, nonzero_chambers, fast_mode)
-                outline_el, SNP = pr_genotype(pr_one_ch, nonzero_chambers, mixed_allele_priors, ref_base, condensed_genotype_set)
+
+                if no_cross_chamber_info:
+                    outline_el, SNP = pr_chambers_BASIC(pr_one_ch, nonzero_chambers, mixed_allele_priors, ref_base, condensed_genotype_set)
+                    tags.append('NO_CROSS_CHAMBER_INFO')
+                else:
+                    outline_el, SNP = pr_genotype(pr_one_ch, nonzero_chambers, ref_base, condensed_genotype_set)
 
                 if SNP:
                     tags.append('SNP')
+
+            for cell in range(n_cells):
+                for chamber in range(n_chambers):
+                    if len(base_data[cell][chamber]) < minimum_coverage:
+                        outline_el[1 + cell*n_chambers + chamber] = '*'
 
             outline_el = [chrom, str(pos+1), ref_base] + outline_el
             tag_info = ';'.join(tags) if tags != [] else 'N/A'
@@ -749,12 +876,13 @@ def call_chamber_alleles(input_file, output_file, boundary_files, SNPs_only=Fals
 
             processed += 1
 
+
 if __name__ == '__main__':
     t1 = time.time()
     args = parseargs()
     n_cells = args.num_cells
 
-    call_chamber_alleles(args.input_file, args.output_file, args.fragment_boundaries, False)
+    call_chamber_alleles(args.input_file, args.output_file, args.fragment_boundaries, SNPs_only=False, no_cross_chamber_info=False)
     t2 = time.time()
 
     print("TOTAL TIME: {} s".format(int(t2-t1)))
